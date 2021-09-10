14e90cfd7cb03ecd900020a80bc96699c1ab7ce4
Garth Kong <kongga2017@gmail.com>
2021-09-09 20:52:37 -0700

    update tracks.sh

--- src/tracks.sh
+++ src/tracks.sh
@@ -16,12 +16,13 @@ N=$(samtools view -c -@ 4 $input)
 sample_name=$(basename $input | cut -d. -f1)
 
 # genome coverage in CPM
-bedtools genomecov -bga -ibam $input | awk -v OFS='\t' -v N=$N '{print $1,$2,$3,$4/N*1e6}' | sort -k1,1 -k2,2n > data/tracks/${sample_name}.bg
-
+bedtools genomecov -bga -ibam $input | awk -v OFS='\t' -v N=$N '{print $1,$2,$3,$4/N*1e6}' | grep -v -E "random|chrUn|chrEBV" | \
 # calculate mean of signal by window
-bedtools map -a $windows -b data/tracks/${sample_name}.bg -c 4 -o mean > data/tracks/${sample_name}.smooth.bg
+bedtools map -a $windows -b stdin -c 4 -o mean | sort --parallel 4 -S 4G -k1,1 -k2,2n > data/tracks/${sample_name}.smooth.bg
 
 # bedgraph to bigwig
+echo "bedGraphToBigWig data/tracks/${sample_name}.smooth.bg $chrom_size $output"
 bedGraphToBigWig data/tracks/${sample_name}.smooth.bg $chrom_size $output
 
-rm data/tracks/${sample_name}.bg data/tracks/${sample_name}.smooth.bg
+rm data/tracks/${sample_name}.smooth.bg
+
