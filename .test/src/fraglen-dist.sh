cdb263da01667d3c33b33fa3ded7347c4fbde66c
Garth Kong <kongga2017@gmail.com>
2022-07-19 14:04:16 -0700

    rm .test in main

--- .test/src/fraglen-dist.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#  fragment length distribution
-samtools view $1 | awk '$9>0' | cut -f 9 | sort | uniq -c | sort -b -k2,2n | sed -e 's/^[ \t]*//' | sed 's/ /\t/g' | awk -v OFS='\t' '{print $2,$1}' > $2
